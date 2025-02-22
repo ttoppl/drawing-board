337746c9660dbed2d96f047958b69e1b2e974a29
ttoppl <78095158+ttoppl@users.noreply.github.com>
2025-02-22 18:03:13 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,21 @@
+const url = `https://drawing-board-vtwi.onrender.com`; // Replace with your Render URL
+const interval = 30000; // Interval in milliseconds (30 seconds)
+
+function reloadWebsite() {
+  axios.get(url)
+    .then(response => {
+      console.log(`Reloaded at ${new Date().toISOString()}: Status Code ${response.status}`);
+    })
+    .catch(error => {
+      console.error(`Error reloading at ${new Date().toISOString()}:`, error.message);
+    });
+}
+
+
+setInterval(reloadWebsite, interval);
+
+
+
 const cors = require("cors");
 const express = require("express");
 const http = require("http");
